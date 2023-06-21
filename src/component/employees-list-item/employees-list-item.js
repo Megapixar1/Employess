6f40ddca299b5d0df1cb81a7cd58869ac5952b2f
Megapixar <mopedo777@gmail.com>
2023-06-21 14:26:33 +0300

    first commit

--- src/component/employees-list-item/employees-list-item.js
+++ src/component/employees-list-item/employees-list-item.js
@@ -5,7 +5,7 @@ const EmployeesListItem = ({name, salary, increase}) => {
     let className = "list-group-item d-flex justify-content-between";
     if (increase) {
         className += ' increase';
-    }
+    }  
 
     return (
         <li className={className}>
